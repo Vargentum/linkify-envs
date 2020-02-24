18816f35d958d650f4992227aa7555a7777b35de
vladyslav.serebriakov <vladislav.serebriakov@x2sy.com>
2020-02-24 14:03:49 +0200

    init commit

---
+++ app/scripts.babel/content.js
@@ -0,0 +1,17 @@
+const linkifyEnvs = () => {
+    document
+        .querySelectorAll('#projectstatus [title^=http')
+        .forEach((node) => {
+            const link = document.createElement('a')
+            link.setAttribute('target', '_blank')
+            link.setAttribute('rel', 'noopener noreferrer')
+            link.setAttribute('href', node.innerHTML)
+            link.innerHTML = node.innerHTML
+            node.parentNode.replaceChild(link, node)
+        })
+}
+
+//on init perform based on chrome strorage value
+window.onload=function(){
+    chrome.storage.sync.get('transform', linkifyEnvs)
+}
